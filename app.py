b9e013837e196a6483f1f05012d1081b69756aaa
arnavshroff <74356348+arnavshroff@users.noreply.github.com>
2021-10-23 22:19:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import requests
 import pandas as pd
 
 
-url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+link = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
-page = requests.get(url)
+page = requests.get(link)
 print(page)
 
 soup = bs(page.text,'html.parser')
@@ -24,7 +24,7 @@ print(temp_list)
 
 
 
-Star_names = []
+star_name = []
 Distance =[]
 Mass = []
 Radius =[]
@@ -32,12 +32,12 @@ Radius =[]
 
 for i in range(1,len(temp_list)):
     
-    Star_names.append(temp_list[i][0])
+    star_name.append(temp_list[i][0])
     Distance.append(temp_list[i][5])
     Mass.append(temp_list[i][7])
     Radius.append(temp_list[i][8])
 
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
-print(df2)
+df = pd.DataFrame(list(zip(star_name,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df)
 
-df2.to_csv('dwarf_stars.csv')
+df.to_csv('dwarf_stars.csv')
